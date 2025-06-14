758b6b580863ca7012dab2aa0cf7c29e1d8905c1
Pavel Ilin <70980291+Pashteticus@users.noreply.github.com>
2025-06-15 01:54:32 +0300

    add device

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -2,8 +2,8 @@ from transformers import AutoTokenizer, AutoModel, AutoModelForCausalLM
 
 
 class ModelEvaluator():
-    def __init__(self, model, tokenizer):
-        self.model = model
+    def __init__(self, model, tokenizer, device="cpu"):
+        self.model = model.to(device)
         self.tokenizer = tokenizer 
 
     def __call__(self, messages):
@@ -14,19 +14,19 @@ class ModelEvaluator():
             tokenize=False,
             add_generation_prompt=True
         )
-        model_inputs = self.tokenizer([text], return_tensors="pt")
+        model_inputs = self.tokenizer([text], return_tensors="pt").to(device)
 
         generated_ids = self.model.generate(
             **model_inputs,
             max_new_tokens=1024
-        )
+        ).detach().cpu()
         generated_ids = [
             output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
         ]
         response = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
         return response
 
-def load_hf_model(model_name):
+def load_hf_model(model_name, device="cpu"):
     if 'gguf' in model_name.lower():
         tokenizer = AutoTokenizer.from_pretrained(
             model_name,
@@ -39,10 +39,10 @@ def load_hf_model(model_name):
         )
     else:
         tokenizer = AutoTokenizer.from_pretrained(model_name)
-        model = AutoModel.from_pretrained(
+        model = AutoModelForCausalLM.from_pretrained(
             model_name
         )
-    return ModelEvaluator(model, tokenizer)
+    return ModelEvaluator(model, tokenizer, device)
 
 def load_local_model(model_name):
     pass
