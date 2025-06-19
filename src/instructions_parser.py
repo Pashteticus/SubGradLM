6371568d149e0de72e3cac8f1a3b0f9237a3573b
Reki <108216546+KhoroshenkoDmitry@users.noreply.github.com>
2025-06-19 15:22:12 +0300

    Add files via upload

--- src/instructions_parser.py
+++ src/instructions_parser.py
@@ -1,16 +1,36 @@
-from utils.parsers_utils import (
-    save_dataset,
-    DsType,
-    format_evol,
-    format_everything,
-    format_open_orca,
-)
-from datasets import load_dataset
+from datasets import load_dataset, DatasetDict
+from utils.parsers_utils import *
 
-FILENAME = "Instructions.csv"
-ds = load_dataset("ise-uiuc/Magicoder-Evol-Instruct-110K")
-save_dataset(ds, DsType.INSTRUCT, FILENAME, format_evol, need_header=True)
-ds = load_dataset("rombodawg/Everything_Instruct")
-save_dataset(ds, DsType.INSTRUCT, FILENAME, format_everything)
-ds = load_dataset("d0rj/OpenOrca-ru")
-save_dataset(ds, DsType.INSTRUCT, FILENAME, format_open_orca)
+
+FILENAME = instructions_filename()
+SIZE = 20 * 1024 * 1024 * 1024
+with load_dataset("ise-uiuc/Magicoder-Evol-Instruct-110K") as ds:
+    index = save_dataset(
+        ds,
+        DsType.INSTRUCT,
+        FILENAME,
+        format_evol,
+        need_header=True,
+        batch_size=100,
+        max_file_size=SIZE / 3,
+    )
+with load_dataset("rombodawg/Everything_Instruct") as ds:
+    index = save_dataset(
+        ds,
+        DsType.INSTRUCT,
+        FILENAME,
+        format_everything,
+        index=index,
+        max_file_size=2 * SIZE / 3,
+        batch_size=100,
+    )
+with load_dataset("d0rj/OpenOrca-ru") as ds:
+    save_dataset(
+        ds,
+        DsType.INSTRUCT,
+        FILENAME,
+        format_open_orca,
+        index=index,
+        max_file_size=SIZE,
+        batch_size=100,
+    )
